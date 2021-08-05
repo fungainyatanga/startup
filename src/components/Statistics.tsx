87d24e3d183bb679b61d783b47296ad5b853381b
fungai nyatanga <fungai.nyatanga16@gmail.com>
2021-08-05 19:55:51 -0400

    added stats section

---
+++ src/components/Statistics.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Statistics = () => {
+  return <div className="stats-section">
+      <div className="stats-1 stats">
+          <h3>25+</h3>
+          <p>Servers</p>
+      </div>
+      <div className="stats-2 stats">
+          <h3>100+</h3>
+          <p>Users</p>
+      </div>
+      <div className="stats-3 stats">
+          <h3>15+ </h3>
+          <p>Commands</p>
+      </div>
+      <div className="stats-4 stats">
+          <h3>∞</h3>
+          <p>Posibilities</p>
+      </div>
+  </div>;
+};
+export default Statistics;
